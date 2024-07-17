ccef4bd3728c2437b688a4e40a8fc2acb3a62cda
Paulo Artur Pinheiro Viana Villaça <112904295+AlgumCorrupto@users.noreply.github.com>
2024-07-17 18:58:43 -0300

    remove funny stuff

--- remove.js
+++ remove.js
@@ -3,13 +3,11 @@
 //    (ev) => {
         let btn = document.getElementsByTagName("button");
         setTimeout(()=>{
-            console.log("fuckyoy")
             for(let i = 0; i < btn.length; i++)
                 {
                     console.log(btn[i].getAttribute("data-testid"))
                     if(btn[i].getAttribute("data-testid") == "upgrade-button")
                     {
-                        console.log("yay")
                         btn[i].remove()
                     }
                 }
